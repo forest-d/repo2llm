0228e5c61e79102676262ece09ee261c8707da48
forest-d <forestdussault@gmail.com>
2024-11-11 18:56:51 -0500

    More ignore targets

--- repo2llm/constants.py
+++ repo2llm/constants.py
@@ -18,8 +18,7 @@ DEFAULT_IGNORE_PATTERNS = {
     '.next',
     '.out',
     '.vercel',
-    '.envs'
-    'locale',
+    '.envs' 'locale',
     'package-lock.json',
     'static',
     # File patterns to ignore
